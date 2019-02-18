5ea17af67a915c46359112f50b94ea199ef77b31
Svetlin Ralchev <svet@ralch.com>
2019-02-18 19:29:05 +0100

    Improve Rollbar test coverage

--- handler/rollbar/rollbar_test.go
+++ handler/rollbar/rollbar_test.go
@@ -4,9 +4,10 @@ import (
 	"time"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
-	log "github.com/phogolabs/log"
+	"github.com/phogolabs/log"
 	"github.com/phogolabs/log/fake"
 	"github.com/phogolabs/log/handler/rollbar"
 )
@@ -25,11 +26,18 @@ var _ = Describe("Rollbar", func() {
 		}
 	})
 
-	It("sends the log to rollbar", func() {
+	Describe("New", func() {
+		It("creates the logger successfully", func() {
+			config := &rollbar.Config{}
+			Expect(rollbar.New(config)).NotTo(BeNil())
+		})
+	})
+
+	DescribeTable("sends the log", func(llevel log.Level, rlevel string) {
 		entry := &log.Entry{
 			Message:   "hello",
 			Timestamp: time.Now(),
-			Level:     log.InfoLevel,
+			Level:     llevel,
 			Fields: []log.Field{
 				log.F("app", "ginkgo"),
 			},
@@ -41,8 +49,13 @@ var _ = Describe("Rollbar", func() {
 		Expect(client.MessageWithExtrasCallCount()).To(Equal(1))
 
 		level, msg, extras := client.MessageWithExtrasArgsForCall(0)
-		Expect(level).To(Equal("info"))
+		Expect(level).To(Equal(rlevel))
 		Expect(msg).To(Equal("hello"))
 		Expect(extras).To(HaveKeyWithValue("app", "ginkgo"))
-	})
+	},
+		Entry("debug", log.DebugLevel, "debug"),
+		Entry("info", log.InfoLevel, "info"),
+		Entry("warn", log.WarnLevel, "warning"),
+		Entry("panic", log.PanicLevel, "critical"),
+	)
 })
