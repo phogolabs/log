68ea00873941d7ce90a096043259dc3eb277ddfd
Svetlin Ralchev <svet@ralch.com>
2019-10-24 11:53:16 +0300

    Improve test coverage

--- handler/json/handler_test.go
+++ handler/json/handler_test.go
@@ -1,12 +1,12 @@
 package json_test
 
 import (
-	"bytes"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/onsi/gomega/gbytes"
 	"github.com/phogolabs/log"
 	"github.com/phogolabs/log/handler/json"
 )
@@ -14,26 +14,21 @@ import (
 var _ = Describe("Handler", func() {
 
 	var (
-		buffer  *bytes.Buffer
+		buffer  *gbytes.Buffer
 		handler *json.Handler
 	)
 
 	BeforeEach(func() {
-		buffer = &bytes.Buffer{}
-
-		handler = json.NewConfig(&json.Config{
-			Writer:           buffer,
-			DisplayColor:     true,
-			PrettyFormatting: false,
-		})
+		buffer = gbytes.NewBuffer()
+		handler = json.New(buffer)
 	})
 
-	ItEncodesTheEntry := func() {
+	ItEncodesTheEntry := func(level log.Level) {
 		It("encodes the entry as json", func() {
 			entry := &log.Entry{
 				Message:   "hello",
 				Timestamp: time.Now(),
-				Level:     log.InfoLevel,
+				Level:     level,
 				Fields: log.Map{
 					"app": "ginkgo",
 				},
@@ -41,23 +36,53 @@ var _ = Describe("Handler", func() {
 
 			handler.Handle(entry)
 
-			m := unmarshal(buffer)
-			Expect(m).To(HaveKeyWithValue("message", "hello"))
-			Expect(m).To(HaveKeyWithValue("level", "INFO"))
+			Expect(buffer).To(gbytes.Say(level.String()))
 		})
 	}
 
-	ItEncodesTheEntry()
+	ItHandlesTheLevels := func() {
+		Describe("Handle", func() {
+			Context("when the level is INFO", func() {
+				ItEncodesTheEntry(log.InfoLevel)
+			})
 
-	Context("when the pretty is on", func() {
+			Context("when the level is WARN", func() {
+				ItEncodesTheEntry(log.WarnLevel)
+			})
+
+			Context("when the level is NOTICE", func() {
+				ItEncodesTheEntry(log.NoticeLevel)
+			})
+
+			Context("when the level is ERROR", func() {
+				ItEncodesTheEntry(log.ErrorLevel)
+			})
+
+			Context("when the level is PANIC", func() {
+				ItEncodesTheEntry(log.PanicLevel)
+			})
+
+			Context("when the level is ALERT", func() {
+				ItEncodesTheEntry(log.AlertLevel)
+			})
+
+			Context("when the level is FATAL", func() {
+				ItEncodesTheEntry(log.FatalLevel)
+			})
+		})
+	}
+
+	ItHandlesTheLevels()
+
+	Context("when the pretty and color are on", func() {
 		BeforeEach(func() {
 			handler = json.NewConfig(&json.Config{
 				Writer:           buffer,
-				DisplayColor:     false,
+				DisplayColor:     true,
 				PrettyFormatting: true,
 			})
 		})
 
-		ItEncodesTheEntry()
+		ItHandlesTheLevels()
 	})
 })
--- handler/json/suite_test.go
+++ handler/json/suite_test.go
@@ -1,21 +1,13 @@
 package json_test
 
 import (
-	"encoding/json"
-	"io"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-func TestJson(t *testing.T) {
+func TestJSON(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Json Suite")
-}
-
-func unmarshal(reader io.Reader) map[string]interface{} {
-	m := make(map[string]interface{})
-	Expect(json.NewDecoder(reader).Decode(&m)).To(Succeed())
-	return m
+	RunSpecs(t, "JSON Suite")
 }
