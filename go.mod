9518a8f20f6b6e6565dda8a41bc989913e18f28f
Svetlin Ralchev <svet@ralch.com>
2020-02-21 22:35:38 +0000

    Add stackdriver support

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.7.0
 	github.com/go-playground/ansi v2.1.0+incompatible
-	github.com/golang/protobuf v1.3.0 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.4 // indirect
@@ -17,9 +16,8 @@ require (
 	github.com/rogpeppe/fastuuid v1.2.0
 	github.com/rollbar/rollbar-go v1.2.0
 	github.com/tj/go-kinesis v0.0.0-20171128231115-08b17f58cb1b
-	golang.org/x/sync v0.0.0-20190227155943-e225da77a7e6 // indirect
 	golang.org/x/sys v0.0.0-20191018095205-727590c5006e // indirect
-	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2 // indirect
+	google.golang.org/api v0.17.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
 
---
+++ handler/stackdriver/handler.go
@@ -0,0 +1,70 @@
+package stackdriver
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/phogolabs/log"
+	logging "google.golang.org/api/logging/v2"
+)
+
+// Config is the configuration of the handler
+type Config struct {
+	Writer io.Writer
+}
+
+var _ log.Handler = &Handler{}
+
+// Handler implementation.
+type Handler struct {
+	writer io.Writer
+}
+
+// New returns the default implementation of a Client.
+func New(config *Config) *Handler {
+	return &Handler{
+		writer: config.Writer,
+	}
+}
+
+// Handle handles the log entry
+func (h *Handler) Handle(e *log.Entry) {
+	entry := logging.LogEntry{
+		Timestamp:   e.Timestamp.Format(time.RFC3339),
+		TextPayload: e.Message,
+		Severity:    h.severity(e.Level),
+	}
+
+	if data, err := json.Marshal(&e.Fields); err == nil {
+		entry.JsonPayload = data
+	}
+
+	if data, err := json.Marshal(&entry); err == nil {
+		fmt.Fprintln(h.writer, string(data))
+	}
+}
+
+func (h *Handler) severity(level log.Level) string {
+	switch level {
+	case log.InfoLevel:
+		return "INFO"
+	case log.DebugLevel:
+		return "DEBUG"
+	case log.NoticeLevel:
+		return "NOTICE"
+	case log.WarnLevel:
+		return "WARNING"
+	case log.ErrorLevel:
+		return "ERROR"
+	case log.PanicLevel:
+		return "CRITICAL"
+	case log.AlertLevel:
+		return "ALERT"
+	case log.FatalLevel:
+		return "FATAL"
+	default:
+		return "DEFAULT"
+	}
+}
