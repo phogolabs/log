5e5ab2710ea9728d67fd75be88694fdd3b53ee09
Svetlin Ralchev <svet@ralch.com>
2020-02-23 02:50:30 +0000

    Fix duplicated jsonPayload fields

--- handler/stackdriver/handler.go
+++ handler/stackdriver/handler.go
@@ -8,8 +8,54 @@ import (
 	"time"
 
 	"github.com/phogolabs/log"
-	logging "google.golang.org/api/logging/v2"
 )
+
+// LogEntry represents the Google Cloud Log Entry
+type LogEntry struct {
+	// TextPayload: The log entry payload, represented as a Unicode string
+	// (UTF-8).
+	TextPayload string `json:"textPayload,omitempty"`
+
+	// Timestamp: Optional. The time the event described by the log entry
+	// occurred. This time is used to compute the log entry's age and to
+	// enforce the logs retention period. If this field is omitted in a new
+	// log entry, then Logging assigns it the current time. Timestamps have
+	// nanosecond accuracy, but trailing zeros in the fractional seconds
+	// might be omitted when the timestamp is displayed.Incoming log entries
+	// should have timestamps that are no more than the logs retention
+	// period in the past, and no more than 24 hours in the future. Log
+	// entries outside those time boundaries will not be available when
+	// calling entries.list, but those log entries can still be exported
+	// with LogSinks.
+	Timestamp string `json:"timestamp,omitempty"`
+
+	// Trace: Optional. Resource name of the trace associated with the log
+	// entry, if any. If it contains a relative resource name, the name is
+	// assumed to be relative to //tracing.googleapis.com. Example:
+	// projects/my-projectid/traces/06796866738c859f2f19b7cfb3214824
+	Trace string `json:"trace,omitempty"`
+
+	// Severity: Optional. The severity of the log entry. The default value
+	// is LogSeverity.DEFAULT.
+	//
+	// Possible values:
+	//   "DEFAULT" - (0) The log entry has no assigned severity level.
+	//   "DEBUG" - (100) Debug or trace information.
+	//   "INFO" - (200) Routine information, such as ongoing status or
+	// performance.
+	//   "NOTICE" - (300) Normal but significant events, such as start up,
+	// shut down, or a configuration change.
+	//   "WARNING" - (400) Warning events might cause problems.
+	//   "ERROR" - (500) Error events are likely to cause problems.
+	//   "CRITICAL" - (600) Critical events cause more severe problems or
+	// outages.
+	//   "ALERT" - (700) A person must take an action immediately.
+	//   "EMERGENCY" - (800) One or more systems are unusable.
+	Severity string `json:"severity,omitempty"`
+
+	// Fields represents the associated fields
+	Fields log.Map `json:"fields,omitempty"`
+}
 
 // Config is the configuration of the handler
 type Config struct {
@@ -35,9 +81,10 @@ func New(config *Config) *Handler {
 
 // Handle handles the log entry
 func (h *Handler) Handle(e *log.Entry) {
-	entry := logging.LogEntry{
+	entry := LogEntry{
 		Timestamp:   e.Timestamp.Format(time.RFC3339),
 		TextPayload: e.Message,
+		Fields:      e.Fields,
 		Severity:    h.severity(e.Level),
 	}
 
@@ -49,10 +96,6 @@ func (h *Handler) Handle(e *log.Entry) {
 				}
 			}
 		}
-	}
-
-	if data, err := json.Marshal(&e.Fields); err == nil {
-		entry.JsonPayload = data
 	}
 
 	if data, err := json.Marshal(&entry); err == nil {
